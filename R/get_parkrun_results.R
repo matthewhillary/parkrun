77f63bad804aab84d12fde1603fe6c2d0424093f
Matthew Hillary <mattnz7777@gmail.com>
2025-05-03 09:48:19 +1200

    automatic update

--- R/get_parkrun_results.R
+++ R/get_parkrun_results.R
@@ -6,9 +6,9 @@
 #' @param athletes
 get_parkrun_results <- function(athletes) {
 
-  athletes %>%
-    tibble::enframe(value = "athlete_id", name = "athlete_name") %>%
-    mutate(dat = map(athlete_id, scrape_parkrun_results_live)) %>%
+  athletes |>
+    tibble::enframe(value = "athlete_id", name = "athlete_name") |>
+    dplyr::mutate(dat = purrr::map(athlete_id, scrape_parkrun_results_live)) |>
     unnest(dat)
 
 }
